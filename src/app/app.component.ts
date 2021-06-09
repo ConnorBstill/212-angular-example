ad4df8ac828966f1a8e50d481c4046e33279d14a
ConnorBStill <connor@lamarsoftware.io>
2021-06-08 17:53:28 -0700

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-example';
+  title = 'asdf';
+
+  firstInteger: number = 0;
+  secondInteger: number = 0;
+
+  placeholder: string = '';
+
+  addTwoNumbers(a: number, b: number) {
+    return a + b;
+  }
+
+  resetNumbers(): void {
+    this.firstInteger = 0;
+    this.secondInteger = 0;
+  }
+
+  returnFirstInteger() {
+    console.log('returnFirstInteger')
+    return this.firstInteger
+  }
+
+  returnSecondInteger() {
+    console.log('returnSecondInteger')
+    return this.secondInteger
+  }
 }
