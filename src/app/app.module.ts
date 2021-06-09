a9b2d1f2bb44bca902ad27b9bbaa8378ddbe4a88
ConnorBStill <connor@lamarsoftware.io>
2021-06-08 19:24:00 -0700

    Add compoennt example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
